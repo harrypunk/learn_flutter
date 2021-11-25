2aa634d9565f53158ce8dc57d66b7ec241f707f4
harry <allthatjazz@outlook.com>
2021-11-25 12:42:03 +0800

    Basic example of mobx counter

---
+++ lib/main.dart
@@ -0,0 +1,63 @@
+import 'dart:developer';
+
+import 'package:flutter/material.dart';
+import 'package:flutter_mobx/flutter_mobx.dart';
+import 'package:provider/provider.dart';
+
+import 'counter.dart'; // Import the Counter
+
+void main() => runApp(const MyApp());
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MultiProvider(
+        providers: [Provider<Counter>(create: (_) => Counter())],
+        child: MaterialApp(
+          title: 'MobX',
+          theme: ThemeData(
+            primarySwatch: Colors.orange,
+          ),
+          home: const MyHomePage(),
+        ));
+  }
+}
+
+class MyHomePage extends StatelessWidget {
+  const MyHomePage({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    final counter = Provider.of<Counter>(context);
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('MobX Counter'),
+      ),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            const Text(
+              'You have pushed the button this many times:',
+            ),
+            // Wrapping in the Observer will automatically re-render on changes to counter.value
+            Observer(
+              builder: (_) => Text(
+                '${counter.value}',
+                style: Theme.of(context).textTheme.headline4,
+              ),
+            ),
+          ],
+        ),
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: counter.increment,
+        tooltip: 'Increment',
+        child: const Icon(Icons.add),
+      ),
+    );
+  }
+}
